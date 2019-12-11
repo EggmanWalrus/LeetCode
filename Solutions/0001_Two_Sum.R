9bb29e0d0f54e3b9a7f4feb64c4c1e76eaf8a470
Yanwen Wang <yanwenwang@win7-5819.icjia.org>
2019-12-11 10:03:36 -0600

    Solved with built-in list function

--- Solutions/0001_Two_Sum.R
+++ Solutions/0001_Two_Sum.R
@@ -1,11 +1,10 @@
 library(hash)
-library(microbenchmark)
 
 nums <- c(2, 3, 4, 5, 7, 11, 15)
 target <- 13
 
 #Loop
-twoSum <- function(nums, target) {
+twoSum_loop <- function(nums, target) {
   for (i in seq(1, length(nums) - 1)) {
     
     for (j in seq(i+1, length(nums))) {
@@ -16,7 +15,7 @@ twoSum <- function(nums, target) {
   }
 }
 
-twoSum(nums, target)
+twoSum_loop(nums, target)
 
 #Hash
 h <- hash()
@@ -32,5 +31,16 @@ twoSum_hash <- function(nums, target) {
 
 twoSum_hash(nums, target)
 
-microbenchmark('loop' = {twoSum(nums, target)},
-               'hash' = {twoSum_hash(nums, target)})
+#List
+l <- list()
+twoSum_list <- function(nums, target) {
+  for (i in seq(1, length(nums))) {
+    if (!((target - nums[i]) %in% unlist(l))) {
+      l[[nums[i]]] <- i
+    } else {
+      print(c(l[[target - nums[i]]], i))
+    }
+  }
+}
+
+twoSum_list(nums, target)
